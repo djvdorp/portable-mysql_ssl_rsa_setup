cebb526958241fc470e309343f06843a25914438
Daniel van Dorp <daniel@vandorp.biz>
2016-09-21 21:39:30 +0200

    start trying to use certstrap

--- main.go
+++ main.go
@@ -5,22 +5,54 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"fmt"
 	"log"
 	"os"
+
+	depot "github.com/square/certstrap/depot"
+	pkix "github.com/square/certstrap/pkix"
 )
 
 func main() {
 	reader := rand.Reader
 	bitSize := 2048
-	key, err := rsa.GenerateKey(reader, bitSize)
+	rsaKey, err := rsa.GenerateKey(reader, bitSize)
 	checkError(err)
 
-	privateKey := x509.MarshalPKCS1PrivateKey(key)
+	privateKey := x509.MarshalPKCS1PrivateKey(rsaKey)
 	savePEMKey("private_key.pem", privateKey, "RSA PRIVATE KEY")
 
-	publicKey, err := x509.MarshalPKIXPublicKey(&key.PublicKey)
+	publicKey, err := x509.MarshalPKIXPublicKey(&rsaKey.PublicKey)
 	checkError(err)
 	savePEMKey("public_key.pem", publicKey, "PUBLIC KEY")
+
+	// certstrap code
+	d, err := depot.NewFileDepot("")
+	checkError(err)
+	commonName := "common-name"
+	//formattedName := strings.Replace(commonName, " ", "_", -1)
+	var pkixKey *pkix.Key
+	pkixKey, err = pkix.CreateRSAKey(2048)
+	if err != nil {
+		log.Fatal("Create RSA Key error:", err)
+	}
+	if err = depot.PutPrivateKey(d, "ca-key", pkixKey); err != nil {
+		fmt.Fprintln(os.Stderr, "Save private key error:", err)
+	}
+	unit := ""
+	years := 10
+	org := ""
+	country := ""
+	province := ""
+	locality := ""
+	crt, err := pkix.CreateCertificateAuthority(pkixKey, unit, years, org, country, province, locality, commonName)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Create certificate error:", err)
+		os.Exit(1)
+	}
+	if err = depot.PutCertificate(d, "ca", crt); err != nil {
+		fmt.Fprintln(os.Stderr, "Save certificate error:", err)
+	}
 }
 
 func savePEMKey(fileName string, keyBytes []byte, keyType string) {
